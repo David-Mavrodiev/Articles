f741ecab9b750909ecbb146c1a4dce24d862e628
David-Mavrodiev <David-Mavrodiev>
2017-10-26 00:35:35 +0300

    Add gulp js transformation

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,49 @@
 'use strict';
  
-var gulp = require('gulp');
-var sass = require('gulp-sass');
- 
-gulp.task('sass', function () {
-  return gulp.src('./source/public/styles/**/*.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./build/public/styles'));
+const gulp = require('gulp'),
+      gulpsync = require("gulp-sync")(gulp),
+      sass = require('gulp-sass'),
+      babel = require("gulp-babel"),
+      clean = require("gulp-clean");
+
+gulp.task("clean", function() {
+    return gulp
+        .src("build", {
+            read: false,
+        })
+        .pipe(clean());
 });
 
-gulp.task('sass:watch', function () {
-  gulp.watch('./source/public/styles/**/*.scss', ['sass']);
+gulp.task('transform:sass', function () {
+  return gulp.src("./source/**/*.scss")
+        .pipe(sass())
+        .pipe(gulp.dest("./build"));
 });
+
+gulp.task('transform:js', () => {
+    return gulp.src('./source/public/**/*.js')
+        .pipe(babel({
+            presets: ['es2015'],
+        }))
+        .pipe(gulp.dest('./build/public'));
+});
+
+gulp.task("copy:all", () => {
+    return gulp
+        .src([
+            "./source/**/*.html",
+            "./source/**/*.js",
+            "./source/**/*.hbs",
+            "./source/**/*.css",
+            "./source/**/*.jpg",
+            "./source/**/*.png",
+            "!./source/public/**/*.js",
+        ])
+        .pipe(gulp.dest("./build"));
+});
+
+gulp.task("transform", ["transform:js", "transform:sass"]);
+
+gulp.task("copy", ["copy:all"]);
+
+gulp.task("build", gulpsync.sync(["clean", "transform", "copy"]));
